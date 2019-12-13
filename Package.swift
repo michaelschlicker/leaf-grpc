05a785f5c7aff2d0df42ef06420daddf8108b56f
Michael Schlicker <michael.schlicker@quickbirdstudios.com>
2019-12-13 13:20:54 +0100

    use revision

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/leaf-kit.git", from: "1.0.0-beta.2"),
-        .package(url: "https://github.com/michaelschlicker/vapor.git", .branch("feature/http-2-only")),
+        .package(url: "https://github.com/michaelschlicker/vapor-grpc.git", .revision("569a1828")),
     ],
     targets: [
         .target(name: "Leaf", dependencies: ["LeafKit", "Vapor"]),
